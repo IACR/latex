1a7f897113d1ae320ac51ba395d06a2fb8ae427a
Kevin McCurley <github@digicrime.com>
2026-07-18 21:46:26 -0700

    Add author roles for issue #302.

--- iacrcc/parser/test_meta_parse.py
+++ iacrcc/parser/test_meta_parse.py
@@ -716,4 +716,8 @@ def test_metacapture():
     assert data['license'] == 'CC-BY-4.0'
     assert len(data['affiliations']) == 2
     assert data['affiliations'][0]['countrycode'] == 'BE'
+    roles = data['authors'][0]['roles']
+    assert roles[0] == 'author'
+    assert roles[1] == 'software'
+    assert len(roles) == 2
     assert data['authors'][1]['email'] == 'latex_test@digicrime.com'
